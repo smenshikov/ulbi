bb557c4bddd49010245599161a7310a0313451e0
smenshikov <menshickowserezha@yandex.ru>
2023-04-17 00:53:43 +0600

    Added not found route

--- src/shared/config/route-config/route-config.tsx
+++ src/shared/config/route-config/route-config.tsx
@@ -1,15 +1,19 @@
 import { RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/main-page';
 import { AboutPage } from 'pages/about-page';
+import { NotFoundPage } from 'pages/not-found-page';
 
 export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
+  NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
+  // Последний роут так как он отработает если не будет найден подходящий роут
+  [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: RouteProps[] = [
@@ -20,5 +24,8 @@ export const routeConfig: RouteProps[] = [
   {
     path: RoutePath[AppRoutes.ABOUT],
     element: <AboutPage />,
+  }, {
+    path: RoutePath[AppRoutes.NOT_FOUND],
+    element: <NotFoundPage />,
   },
 ];
