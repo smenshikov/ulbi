498c9122777aa544e957ee752543f4adc798103e
smenshikov <menshickowserezha@yandex.ru>
2023-04-30 22:45:09 +0600

    Increased max-len rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,7 +37,7 @@ module.exports = {
       'error',
       { markupOnly: true, ignoreAttribute: ['to', 'theme'] },
     ],
-    'max-len': ['error', { ignoreComments: true }],
+    'max-len': ['error', { ignoreComments: true, code: 100 }],
   },
   globals: {
     __IS_DEV__: true,
