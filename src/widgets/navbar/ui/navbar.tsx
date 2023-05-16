58b59c9263edda5684330f9a2d9fcc6e6beb7f70
smenshikov <menshickowserezha@yandex.ru>
2023-05-16 23:11:11 +0600

    Added keys of navbar to translation files

--- src/widgets/navbar/ui/navbar.tsx
+++ src/widgets/navbar/ui/navbar.tsx
@@ -1,21 +1,26 @@
 import classNames from 'classnames';
 import { AppLink } from 'shared/ui/app-link/app-link';
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import styles from './navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar: FC<NavbarProps> = ({ className }) => (
-  <div className={classNames(styles.navbar, className)}>
-    <div className={styles.links}>
-      <AppLink to="/" theme="secondary">
-        Home
-      </AppLink>
-      <AppLink to="/about" theme="secondary">
-        About
-      </AppLink>
+export const Navbar: FC<NavbarProps> = ({ className }) => {
+  const { t } = useTranslation('navbar');
+
+  return (
+    <div className={classNames(styles.navbar, className)}>
+      <div className={styles.links}>
+        <AppLink to="/" theme="secondary">
+          {t('home')}
+        </AppLink>
+        <AppLink to="/about" theme="secondary">
+          {t('about')}
+        </AppLink>
+      </div>
     </div>
-  </div>
-);
+  );
+};
