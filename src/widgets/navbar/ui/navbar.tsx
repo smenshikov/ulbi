fd68b9e9b5785379d8f9b2083bf13110994115f6
smenshikov <menshickowserezha@yandex.ru>
2023-07-02 15:24:50 +0600

    Used modal component

--- src/widgets/navbar/ui/navbar.tsx
+++ src/widgets/navbar/ui/navbar.tsx
@@ -1,15 +1,32 @@
 import classNames from 'classnames';
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
+import { Modal } from 'shared/ui/modal/modal';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/button/button';
 import styles from './navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar: FC<NavbarProps> = ({ className }) => (
-  <div className={classNames(styles.navbar, className)}>
-    <div className={styles.links}>
-      /
+export const Navbar: FC<NavbarProps> = ({ className }) => {
+  const { t } = useTranslation();
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const handleToggleModal = useCallback(() => {
+    setIsAuthModal((prev) => !prev);
+  }, []);
+
+  return (
+    <div className={classNames(styles.navbar, className)}>
+      <Button theme="clearInverted" className={styles.links} onClick={handleToggleModal}>
+        {t('login')}
+      </Button>
+      <Modal isOpen={isAuthModal} onClose={handleToggleModal}>
+        {/* eslint-disable-next-line i18next/no-literal-string */}
+        {/* eslint-disable-next-line i18next/no-literal-string */}
+        qwe
+      </Modal>
     </div>
-  </div>
-);
+  );
+};
